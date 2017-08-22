cfebcbca7b62d659dd72bac17187b6030538ea3b
Daniel Deutsch <deudan1010@gmail.com>
2017-08-22 18:45:33 +0200

    fix display with flexbox

--- src/App.js
+++ src/App.js
@@ -7,14 +7,20 @@ import CollapsibleCon from './components/sideBar/CollapsibleCon';
 class App extends Component {
 	render() {
 		return (
-			<div>
+			<div style={{
+				display: 'flex',
+				minHeight: '100vh',
+				flexDirection: 'column',
+			}}>
 				<Header />
-				<div className="row">
-				<div className="col s12 m3 grey">
-				<CollapsibleCon />
-				</div>
-				<div className="col s12 m9 green">Area for Graph</div>
-				</div>
+				<main className="grey">
+						<div className="row">
+							<div className="col s12 m3">
+								<CollapsibleCon />
+							</div>
+							<div className="col s12 m9 green">Area for Graph</div>
+						</div>
+				</main>
 				<Footer />
 			</div>
 		);
