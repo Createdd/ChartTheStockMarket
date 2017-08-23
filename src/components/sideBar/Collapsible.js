aeeb738f4be057e386062499b4ca47bbec2b5632
Daniel Deutsch <deudan1010@gmail.com>
2017-08-23 17:59:18 +0200

    add functionality to send stock code

--- src/components/sideBar/Collapsible.js
+++ src/components/sideBar/Collapsible.js
@@ -1,25 +1,48 @@
 import React from 'react';
 
-const Collapsible = (props) => {
+const Collapsible = props => {
+	const renderStocks = props.examples.map((elem, ind) => {
+		return (
+			<li key={ind}>
+				<div className="collapsible-header grey darken-3">
+					<i className="fa fa-info-circle material-icons" aria-hidden="true" />
+					{elem.dataset.dataset_code}
+				</div>
+				<div className="collapsible-body grey darken-3">
+					<span>
+						{elem.dataset.name}
+					</span>
+				</div>
+			</li>
+		);
+	});
+
 	return (
 		<ul className="collapsible popout white-text" data-collapsible="accordion">
-    <li>
-      <div className="collapsible-header grey darken-3"><i className="fa fa-info-circle tiny material-icons" aria-hidden="true" />{props.examples[0].dataset.dataset_code}</div>
-      <div className="collapsible-body grey darken-3"><span>{props.examples[0].dataset.name}</span></div>
-    </li>
-    <li>
-      <div className="collapsible-header grey darken-3"><i className="fa fa-info-circle tiny material-icons" aria-hidden="true" />{props.examples[1].dataset.dataset_code}</div>
-      <div className="collapsible-body grey darken-3"><span>{props.examples[1].dataset.name}</span></div>
-    </li>
-    <li>
-      <div className="collapsible-header grey darken-3"><i className="fa fa-info-circle tiny material-icons" aria-hidden="true" />{props.examples[0].dataset.dataset_code}</div>
-      <div className="collapsible-body grey darken-3"><span>{props.examples[0].dataset.name}</span></div>
-    </li>
-    <li>
-      <div className="collapsible-header grey darken-3"><i className="fa fa-info-circle tiny material-icons" aria-hidden="true" />{props.examples[1].dataset.dataset_code}</div>
-      <div className="collapsible-body grey darken-3"><span>{props.examples[1].dataset.name}</span></div>
-    </li>
-    </ul>
+			{renderStocks}
+			<li>
+				<div className="collapsible-header grey darken-3">
+					<i className="fa fa-plus material-icons" aria-hidden="true" />Add
+					Stock
+				</div>
+				<div className="collapsible-body grey darken-3">
+					<form className="input-field" onSubmit={(e) => props.addStock(e)}>
+						<i
+							className="fa fa-paper-plane material-icons waves-effect waves-light blue-grey-text darken-1 prefix"
+							aria-hidden="true"
+							onClick={(e) => props.addStock(e)}
+						/>
+						<input
+							id="stockCode"
+							type="text"
+							value={props.value}
+              onChange={e => props.onChange(e)}
+						/>
+						<label htmlFor="stockCode">Stock Code</label>
+					</form>
+				</div>
+			</li>
+		</ul>
 	);
 };
 
--- src/components/sideBar/CollapsibleCon.js
+++ src/components/sideBar/CollapsibleCon.js
@@ -7,10 +7,28 @@ export default class CollapsibleCon extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			examples: [FB, TSLA]
+			examples: [FB, TSLA],
+			value: ''
 		};
+		this.addStock = this.addStock.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+	handleChange(e) {
+		this.setState({ value: e.target.value });
+	}
+	addStock(e) {
+		e.preventDefault();
+		console.log('Sent stock Code: ' + this.state.value);
+		this.setState({ value: "" });
 	}
 	render() {
-		return <Collapsible examples={this.state.examples} />;
+		return (
+			<Collapsible
+				examples={this.state.examples}
+				addStock={this.addStock}
+				onChange={this.handleChange}
+				value={this.state.value}
+			/>
+		);
 	}
 }
