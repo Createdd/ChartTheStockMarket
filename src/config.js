2e3ed1e1587ba58c0aa37bec147aca85b9ea4bb7
Daniel Deutsch <deudan1010@gmail.com>
2017-08-24 15:27:45 +0200

    add config

---
+++ src/config.js
@@ -0,0 +1,19 @@
+import * as toastr from 'toastr';
+
+export const toastOptions = toastr.options = {
+  "closeButton": false,
+  "debug": false,
+  "newestOnTop": true,
+  "progressBar": true,
+  "positionClass": "toast-top-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "2000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
