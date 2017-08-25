60dec4a777c88c3af7430106903309e79f0c0ec3
Daniel Deutsch <deudan1010@gmail.com>
2017-08-25 14:35:19 +0200

    add dynamic crosshair

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -10,58 +10,82 @@ import {
 	Borders,
 	GradientDefs,
 	linearGradient,
-	makeWidthFlexible
+	makeWidthFlexible,
+	Crosshair
 } from 'react-vis';
 
-const gradient = (
-	<GradientDefs>
-		<linearGradient id="borderGradient" gradientUnits="userSpaceOnUse">
-			<stop offset="10%" stopColor="#455a64" stopOpacity={0.3} />
-			<stop offset="33%" stopColor="#546e7a" stopOpacity={0.3} />
-			<stop offset="66%" stopColor="#37474f" stopOpacity={0.3} />
-			<stop offset="90%" stopColor="#263238" stopOpacity={0.3} />
-		</linearGradient>
-	</GradientDefs>
-);
-const renderLines = props => {
-	return props.data.map((line, ind) => {
-		return <LineMarkSeries data={line} color={'#' + (Math.random().toString(16) + "000000").substring(2,8)} size={0.1} key={ind} />;
-	});
-};
+export default class Chart extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			crosshairValues: []
+		};
+	}
 
-const renderLongestTimeperiod = (props) => {
-	props.tickValues.sort();
+	render() {
+		const gradient = (
+			<GradientDefs>
+				<linearGradient id="borderGradient" gradientUnits="userSpaceOnUse">
+					<stop offset="10%" stopColor="#455a64" stopOpacity={0.3} />
+					<stop offset="33%" stopColor="#546e7a" stopOpacity={0.3} />
+					<stop offset="66%" stopColor="#37474f" stopOpacity={0.3} />
+					<stop offset="90%" stopColor="#263238" stopOpacity={0.3} />
+				</linearGradient>
+			</GradientDefs>
+		);
+		const renderLines = props => {
+			return props.data.map((line, ind) => {
+				return (
+					<LineMarkSeries
+						data={line}
+						color={
+							'#' + (Math.random().toString(16) + '000000').substring(2, 8)
+						}
+						size={0.1}
+						key={ind}
+						onNearestX={(value, {index}) =>
+						this.setState({crosshairValues: props.data.map(d => d[index])})}
+					/>
+				);
+			});
+		};
+		const renderLongestTimeperiod = props => {
+			props.tickValues.sort();
+		};
+
+		const Plot = ({ width, props }) =>
+			<XYPlot
+				onMouseLeave={() => this.setState({ crosshairValues: [] })}
+				height={500}
+				width={width}
+				style={{ backgroundColor: '#c2c4c6' }}
+			>
+				<HorizontalGridLines />
+				<VerticalGridLines />
+				{renderLongestTimeperiod(props)}
+				<XAxis
+					tickValues={props.tickValues[0]}
+					tickFormat={d =>
+						`${new Date(d).getMonth()}-${new Date(d).getFullYear()}`}
+					title="Date"
+				/>
+				<YAxis title="Value" />
+				{renderLines(props)}
+				{gradient}
+				<Borders
+					style={{
+						right: { fill: '#c2c4c6' },
+						top: { fill: '#c2c4c6' },
+						bottom: { fill: 'url(#borderGradient)' },
+						left: { fill: 'url(#borderGradient)' }
+					}}
+				/>
+				<Crosshair values={this.state.crosshairValues}/>
+			</XYPlot>;
+
+		Plot.propTypes = { width: PropTypes.number, measurements: PropTypes.array };
+		Plot.displayName = 'TimeSeriesLineChartPlot';
+		const FlexibleXYPlot = makeWidthFlexible(Plot);
+		return <FlexibleXYPlot props={this.props} />;
+	}
 }
-
-const Plot = ({ width, props }) =>
-	<XYPlot height={500} width={width} style={{ backgroundColor: '#c2c4c6' }}>
-		<HorizontalGridLines />
-		<VerticalGridLines />
-		{renderLongestTimeperiod(props)}
-		<XAxis
-			tickValues={props.tickValues[0]}
-			tickFormat={d => `${new Date(d).getMonth()}-${new Date(d).getFullYear()}`}
-			title="Date"
-		/>
-		<YAxis title="Value" />
-		{renderLines(props)}
-		{gradient}
-		<Borders
-			style={{
-				right: { fill: '#c2c4c6' },
-				top: { fill: '#c2c4c6' },
-				bottom: { fill: 'url(#borderGradient)' },
-				left: { fill: 'url(#borderGradient)' }
-			}}
-		/>
-	</XYPlot>;
-
-Plot.propTypes = { width: PropTypes.number, measurements: PropTypes.array };
-Plot.displayName = 'TimeSeriesLineChartPlot';
-const FlexibleXYPlot = makeWidthFlexible(Plot);
-
-const Chart = props => {
-	return <FlexibleXYPlot props={props} />;
-};
-
-export default Chart;
