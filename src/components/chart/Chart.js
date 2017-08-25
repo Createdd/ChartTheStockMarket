2e35e55878b471e34466c386517f28c5cc5fbb7c
Daniel Deutsch <deudan1010@gmail.com>
2017-08-25 11:15:09 +0200

    update chart styling with gradient and borders

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -7,11 +7,28 @@ import {
 	XYPlot,
 	YAxis,
 	LineMarkSeries,
+	Borders,
+	GradientDefs,
+	linearGradient,
 	makeWidthFlexible
 } from 'react-vis';
 
+const gradient = (
+	<GradientDefs>
+		<linearGradient
+			id="borderGradient"
+			gradientUnits="userSpaceOnUse"
+		>
+			<stop offset="10%" stopColor="#455a64" stopOpacity={0.3} />
+			<stop offset="33%" stopColor="#546e7a" stopOpacity={0.3} />
+			<stop offset="66%" stopColor="#37474f" stopOpacity={0.3} />
+			<stop offset="90%" stopColor="#263238" stopOpacity={0.3} />
+		</linearGradient>
+	</GradientDefs>
+);
+
 const Plot = ({ width, props }) =>
-	<XYPlot height={500} width={width} style={{backgroundColor: "c2c4c6"}}  >
+	<XYPlot height={500} width={width} style={{ backgroundColor: '#c2c4c6' }}>
 		<HorizontalGridLines />
 		<VerticalGridLines />
 		<XAxis
@@ -20,12 +37,20 @@ const Plot = ({ width, props }) =>
 			title="Date"
 		/>
 		<YAxis title="Value" />
-		<LineMarkSeries data={props.data} color="green" size={0.1} />
+		<LineMarkSeries data={props.data} color={'url(#borderGradient)'} size={0.1} />
+		{gradient}
+		<Borders
+			style={{
+				right: {fill: '#c2c4c6'},
+				top: {fill: '#c2c4c6'},
+				bottom: { fill: 'url(#borderGradient)' },
+				left: { fill: 'url(#borderGradient)' }
+			}}
+		/>
 	</XYPlot>;
 
-
-  Plot.propTypes = { width: PropTypes.number, measurements: PropTypes.array }
-  Plot.displayName = 'TimeSeriesLineChartPlot'
+Plot.propTypes = { width: PropTypes.number, measurements: PropTypes.array };
+Plot.displayName = 'TimeSeriesLineChartPlot';
 const FlexibleXYPlot = makeWidthFlexible(Plot);
 
 const Chart = props => {
