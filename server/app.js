8336b3a121c051bf047bf23f1e1fe3720ec5a274
Daniel Deutsch <deudan1010@gmail.com>
2017-09-05 15:44:00 +0200

    connect to mongolab

--- server/app.js
+++ server/app.js
@@ -4,10 +4,26 @@ const path = require('path');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 app.use(morgan('dev'));
-
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var db = mongoose.connection.openUri((
+	`mongodb://${process.env.REACT_APP_MONGO_USER}:${process.env
+		.REACT_APP_MONGO_PASS}@ds125774.mlab.com:25774/chartstockmarket`
+));
+db.on('error', (err) => {
+  console.log('FAILED to connect to mongoose');
+  console.error(err);
+});
+db.once('open', () => {
+	console.log('connected to mongoose');
+});
 
 app.get('/', (req, res) => {
 	res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
