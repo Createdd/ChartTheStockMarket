2a63a12b22273a78bf7884235a4b1b390c389bc9
Daniel Deutsch <deudan1010@gmail.com>
2017-09-05 14:50:04 +0200

    add socket io on server and client

--- server/app.js
+++ server/app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.use(morgan('dev'));
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+app.get('/', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
-module.exports = app;
+io.on('connection', function(socket) {
+	console.log('New client connected');
+	socket.emit('stocks', { stocks: ['GOOGL', 'TSLA'] });
+	socket.on('my other event', function(data) {
+		console.log(data);
+	});
+	socket.on('disconnect', function() {
+		console.log('Client disconnected');
+	});
+});
+
+module.exports = server;
--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import socketIOClient from "socket.io-client";
+
 
 import Header from './Header';
 import Footer from './Footer';
@@ -6,6 +8,19 @@ import CollapsibleCon from './sideBar/CollapsibleCon';
 import ChartCon from './chart/ChartCon';
 
 class App extends Component {
+	constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://127.0.0.1:9000"
+    };
+	}
+	componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+		socket.on("stocks", data => this.setState({ response: data }));
+		console.log(this.state.response);
+  }
 	render() {
 		return (
 			<div
